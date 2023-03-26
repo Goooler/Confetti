abe7e717a7bd402a7bf944272b579fe89e138317
Goooler <wangzongler@gmail.com>
2023-03-26 19:39:49 +0800

    Enable build scan publishing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -38,6 +38,18 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.gradle.enterprise") version "3.12.6"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
+
 rootProject.name = "Confetti"
 include(":androidApp")
 include(":androidBenchmark")
